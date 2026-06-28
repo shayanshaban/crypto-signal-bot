56ea06c323f868ef90af8b6d20fd25b68fd4103d
shayan986 <shayansh1324@gmail.com>
2026-06-28 16:43:21 +0330

    update extractor

--- src/data/data_extractor.py
+++ src/data/data_extractor.py
@@ -1,12 +1,13 @@
+import json
 import zipfile
 from pathlib import Path
 
 import pandas as pd
-from src.data.baker import enrich_dataframe
+
 import config
-from src.db            import manager as db
-import json
-from pathlib import Path
+from src.data.baker import enrich_dataframe
+from src.db import manager as db
+
 
 STATE_FILE = "import_state.json"
 
@@ -32,8 +33,9 @@ def save_import_state(folder, imported):
         json.dump(
             {"imported_files": sorted(imported)},
             f,
-            indent=4
+            indent=4,
         )
+
 
 folder = config.IMPORT_DATA_FOLDER_DIR
 
@@ -41,6 +43,7 @@ folder = config.IMPORT_DATA_FOLDER_DIR
 def import_zip_folder(folder: str):
     folder = Path(folder)
     imported = load_import_state(folder)
+
     for zip_path in folder.glob("*.zip"):
         if zip_path.name in imported:
             print(f"Skip {zip_path.name}")
@@ -49,16 +52,34 @@ def import_zip_folder(folder: str):
         print(f"Importing {zip_path.name}")
 
         with zipfile.ZipFile(zip_path) as z:
-
             csv_name = next(name for name in z.namelist() if name.endswith(".csv"))
 
             with z.open(csv_name) as f:
-                df = pd.read_csv(f)
+                # بدون فرض وجود هدر بخوان
+                df = pd.read_csv(f, header=None)
 
-        
+                # اگر فایل هدر داشته باشد
+                if str(df.iloc[0, 0]).strip().lower() == "open_time":
+                    df.columns = df.iloc[0]
+                    df = df.iloc[1:].reset_index(drop=True)
+                    df = df[
+                        ["open_time", "open", "high", "low", "close", "volume"]
+                    ]
+                else:
+                    # فایل هدر ندارد
+                    df = df.iloc[:, :6]
+                    df.columns = [
+                        "open_time",
+                        "open",
+                        "high",
+                        "low",
+                        "close",
+                        "volume",
+                    ]
+
         candles = [
             (
-                int(row.open_time) // 1000,   # timestamp -> second
+                int(row.open_time) // 1000,
                 float(row.open),
                 float(row.high),
                 float(row.low),
@@ -70,8 +91,9 @@ def import_zip_folder(folder: str):
 
         db.save_historical_candel(
             candles,
-            config.TRADING_TIME_FRAME
+            config.TRADING_TIME_FRAME,
         )
+
         imported.add(zip_path.name)
         save_import_state(folder, imported)
 
@@ -79,8 +101,12 @@ def import_zip_folder(folder: str):
 
     db.rebuild_baseline_from_historical(config.TRADING_TIME_FRAME)
 
-    print("Enriching Data....(It Take Several Minutes)")
+    print("Enriching Data.... (It Takes Several Minutes)")
     candles = db.get_all_baseline()
     df_window = db.candles_to_dataframe(candles)
-    df_window = enrich_dataframe(df_window,True)
-    db.insert_enriched_dataframe(df_window,config.SYMBOL_DISPLAY,config.TRADING_TIME_FRAME)
+    df_window = enrich_dataframe(df_window, True)
+    db.insert_enriched_dataframe(
+        df_window,
+        config.SYMBOL_DISPLAY,
+        config.TRADING_TIME_FRAME,
+    )
