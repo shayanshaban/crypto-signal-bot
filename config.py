0b9b79672b67642464451191dcffb8f01cabf7e8
shayan986 <shayansh1324@gmail.com>
2026-07-17 08:21:27 +0330

    change multipiler atr

--- config.py
+++ config.py
@@ -95,8 +95,8 @@ TRADING_TIME_FRAME = "minute5"
 MIN_STOP_PERCENT = 0.0024   
 ATR_MULTIPLIER = 1.5         
 RR = 6
-ATR_MULTIPLIER_SL = 2
-ATR_MULTIPLIER_TP = 4
+ATR_MULTIPLIER_SL = 1.5
+ATR_MULTIPLIER_TP = 3
 
 CANDLES = {
     "month1":   {"tf_minutes": 43_200, "count": 24, "raw": False, "raw_and_bake": False},
